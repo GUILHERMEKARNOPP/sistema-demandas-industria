b4f7fd580cbf698232c23d258a4b93e68cad4336
GUILHERME KARNOPP <guilherme.karnopp02@gmail.com>
2026-05-01 00:31:29 -0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sistema-demandas-industria/sw.js', { scope: '/sistema-demandas-industria/' })})}
